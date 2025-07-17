c97b58dc92d089e807669715bf34578f2ef76499
Ndzenyuy <ndzenyuyjones@gmail.com>
2025-07-17 23:21:14 +0100

    updated sqs queue endpoint url

--- requestor/app/sqs_client.py
+++ requestor/app/sqs_client.py
@@ -8,6 +8,7 @@ def get_sqs_client():
         region_name=settings.AWS_REGION,
         aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
         aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+        endpoint_url="http://localstack:4566" ,
     )
 
 def send_message_to_queue(message: dict):
