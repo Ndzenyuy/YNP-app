53e07bddb4d8dd08c1eacd992c599f840f35b616
Ndzenyuy <ndzenyuyjones@gmail.com>
2025-07-18 01:08:12 +0100

    trying updates

--- requestor/app/main.py
+++ requestor/app/main.py
@@ -16,7 +16,7 @@ def notify(req: NotificationRequest):
     try:
         response = send_message_to_queue(req.dict())
         logging.info(f"NotificationRequest payload: {response}")
-        print(f"NotificationRequest payload: {response}")
+        print(f"NotificationRequest payload: {req.dict()}")
         return {
             "message_id": response.get("MessageId"),
             "status": "queued"
