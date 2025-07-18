f6f9f8588b368f9793b78ad66ee6aebb3dbfeda4
Ndzenyuy <ndzenyuyjones@gmail.com>
2025-07-18 20:42:42 +0100

    added logging

--- worker/app/main.py
+++ worker/app/main.py
@@ -16,13 +16,18 @@ def run_worker():
                 output = body.get("OutputType")
                 if output == "EMAIL":
                     notifier.send_email(cfg["SES-Domain-ARN"], body["EmailAddresses"], body["Subject"], body["Message"])
+                    print(f"Email sent to {body['EmailAddresses']}")
                 elif output in ["SMS", "PUSH"]:
                     notifier.send_sns(cfg["SNS-Topic-ARN"], body["Message"])
+                    print(f"Notification sent via {output} to {body['PhoneNumber'] or body['PushToken']}")
                 else:
                     raise Exception("Unsupported OutputType")
+                
 
                 dynamodb_client.log_request(app_id, body, "delivered")
+                print(f"Request logged for {app_id}")
                 sqs_client.delete_message(msg["ReceiptHandle"])
+                print(f"Message deleted from SQS: {msg['ReceiptHandle']}")
             except Exception as e:
                 dynamodb_client.log_request(body.get("Application", "unknown"), body, "failed", str(e))
                 logger.log(f"Error: {e}")
