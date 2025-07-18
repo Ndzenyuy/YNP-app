fe3a50c2c310635b85157dde8370bf2e2df8918f
Ndzenyuy <ndzenyuyjones@gmail.com>
2025-07-18 20:59:31 +0100

    added loggin with logger

--- worker/app/main.py
+++ worker/app/main.py
@@ -1,6 +1,7 @@
 import json
 from . import sqs_client, dynamodb_client, notifier, logger
 
+
 def run_worker():
     logger.log("Worker started polling SQS...")
     while True:
@@ -9,6 +10,7 @@ def run_worker():
             try:
                 body = json.loads(msg["Body"])
                 app_id = body["Application"]
+                logger.log(f"Processing message for application: {app_id}")
                 cfg = dynamodb_client.get_application_config(app_id)
                 if not cfg:
                     raise Exception("App config not found")
@@ -16,18 +18,17 @@ def run_worker():
                 output = body.get("OutputType")
                 if output == "EMAIL":
                     notifier.send_email(cfg["SES-Domain-ARN"], body["EmailAddresses"], body["Subject"], body["Message"])
-                    print(f"Email sent to {body['EmailAddresses']}")
+                    logger.log(f"Email sent to {body['EmailAddresses']}")
                 elif output in ["SMS", "PUSH"]:
                     notifier.send_sns(cfg["SNS-Topic-ARN"], body["Message"])
-                    print(f"Notification sent via {output} to {body['PhoneNumber'] or body['PushToken']}")
+                    logger.log(f"Notification sent via {output} to {body['PhoneNumber'] or body['PushToken']}")
                 else:
                     raise Exception("Unsupported OutputType")
-                
 
                 dynamodb_client.log_request(app_id, body, "delivered")
-                print(f"Request logged for {app_id}")
+                logger.log(f"Message processed successfully: {body}")
                 sqs_client.delete_message(msg["ReceiptHandle"])
-                print(f"Message deleted from SQS: {msg['ReceiptHandle']}")
+                logger.log("Message deleted from SQS")
             except Exception as e:
                 dynamodb_client.log_request(body.get("Application", "unknown"), body, "failed", str(e))
                 logger.log(f"Error: {e}")
